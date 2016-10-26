9769c57a2fce830d8ec7cc1b1fe9114dfb4174d4
Matt Bierbaum <matt.bierbaum@gmail.com>
2016-10-26 13:22:27 -0400

    bumping version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(name='loggr',
       license='MIT License',
       author='Matt Bierbaum',
-      version='0.0.3',
+      version='0.0.4',
 
       packages=['loggr'],
       install_requires=["tornado>=4.3"],
