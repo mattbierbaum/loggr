436474b9f49a53ef9a62dc54fef78dfcc3c3d178
Matt Bierbaum <matt.bierbaum@gmail.com>
2016-10-31 13:51:57 -0400

    better metadata

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ A simple Python logging handler which transmits logs to a centralized service
 (also provided) with minimal work from both the web server side and the Python
 side as well. For help, install the package, run loggr.py, and visit
 localhost:8888 for more information.
+
+In the most basic form, simply add another handler to your existing logging
+class via::
+
+    import logging
+    from loggr import LoggrHandler
+
+    handler = LoggrHandler(url='http://someurl/', logname='optional-name')
+    logger = logging.getLogger('root')
+    logger.addHandler(handler)
--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
 #from setuptools import setup
+import os
+import re
 from distutils.core import setup
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+def version():
+    return re.findall(r".*__version__ = \'(.*)\'.*", read('tmpr'))[0]
+
+def readme():
+    return read('README.md')
 
 setup(name='loggr',
       license='MIT License',
       author='Matt Bierbaum',
-      version='0.0.4',
+      url='https://github.com/mattbierbaum/loggr',
+      version=version(),
 
       packages=['loggr'],
       install_requires=["tornado>=4.3"],
       scripts=['bin/loggr']
+
+      description='Remote log platform with easy integration into Python logging.',
+      long_description=readme(),
 )
